a7a1ebb771562c9f55d008222500909738f80f1f
BenderBlog "SuperBart" Rodriguez <Songze@live.cn>
2024-04-09 21:35:47 +0800

    4 range 4 image simulation

--- helperSimulatorScript.m
+++ helperSimulatorScript.m
@@ -1,8 +1,8 @@
 % Initialize random number generator
-rng(8000)
+rng(11111)
 
 % Create terrain
-xLimits         = [900 1200]; % x-axis limits of terrain (m)
+xLimits         = [900 1600]; % x-axis limits of terrain (m)
 yLimits         = [-200 200]; % y-axis limits of terrain (m)
 roughnessFactor = 1.75;       % Roughness factor
 initialHgt      = 0;          % Initial height (m)
@@ -14,8 +14,8 @@ numIter         = 8;          % Number of iterations
 A(A < 0) = 0; % Fill-in areas below 0
 xvec = x(1,:); 
 yvec = y(:,1);
-resMapX = mean(diff(xvec))
-resMapY = mean(diff(yvec))
+resMapX = mean(diff(xvec));
+resMapY = mean(diff(yvec));
 % Plot simulated terrain
 helperPlotSimulatedTerrain(xvec,yvec,A)
 
@@ -24,7 +24,7 @@ freq = 1e9;                        % Carrier frequency (Hz)
 [lambda,c] = freq2wavelen(freq);   % Wavelength (m) 
 bw = 30e6;                         % Signal bandwidth (Hz)
 fs = 60e6;                         % Sampling frequency (Hz)
-tpd = 3e-6;                        % Pulse width (sec) 
+tpd = 3e-6;                        % Pulse width (sec)
 
 % Verify that the range resolution is as expected
 bw2rangeres(bw)
@@ -36,10 +36,7 @@ sqa = 0;                           % Squint angle (deg)
 % Platform properties
 v = 100;                           % Speed of the platform (m/s)
 dur = 2;                           % Duration of flight (s) 
-rdrhgt = 1000;                     % Height of platform (m) 
-rdrpos1 = [0 -v rdrhgt];           % Start position of the radar (m)
-rdrvel = [0 v 0];                  % Radar plaform velocity
-rdrpos2 = rdrvel*dur + rdrpos1;    % End position of the radar (m)
+rdrhgt = 1000;                     % Height of platform (m)
 len = sarlen(v,dur)                % Synthetic aperture length (m)
 
 % Configure the target platforms in x and y
@@ -55,35 +52,6 @@ end
 
 % Set the reference slant range for the cross-range processing
 rc = sqrt((rdrhgt - mean(tgthgts))^2 + (mean(targetpos(:,1)))^2);
-
-% Antenna orientation
-depang = depressionang(rdrhgt,rc,'Flat','TargetHeight',mean(tgthgts)); % Depression angle (deg)
-grazang = depang; % Grazing angle (deg)
-
-% Azimuth resolution
-azResolution = sarazres(rc,lambda,len)  % Cross-range resolution (m)
-
-% Determine PRF bounds 
-[swlen,swwidth] = aperture2swath(rc,lambda,apertureLength,grazang);
-[prfmin,prfmax] = sarprfbounds(v,azResolution,swlen,grazang)
-
-% Select a PRF within the PRF bounds
-prf = 500; % Pulse repetition frequency (Hz)
-
-% Create a radar scenario
-scene = radarScenario('UpdateRate',prf,'IsEarthCentered',false,'StopTime',dur);
-
-% Add platforms to the scene using the configurations previously defined 
-rdrplat = platform(scene,'Trajectory',kinematicTrajectory('Position',rdrpos1,'Velocity',[0 v 0]));
-
-% Add target platforms
-rcs = rcsSignature('Pattern',5); 
-for it = 1:3
-    platform(scene,'Position',targetpos(it,:),'Signatures',{rcs});
-end
-
-% Plot ground truth
-helperPlotGroundTruth(xvec,yvec,A,rdrpos1,rdrpos2,targetpos)
 
 % Specify custom reflectivity map
 grazTable = 20:0.1:60;
@@ -104,79 +72,134 @@ reflectivityMap = surfaceReflectivity('Custom','Frequency',freqTable, ...
     'GrazingAngle',grazTable,'Reflectivity',reflectivityLayers, ...
     'Speckle','Rayleigh')
 
-% Add land surface to scene
-s = landSurface(scene,'Terrain',A,'Boundary',[xLimits;yLimits], ...
-    'RadarReflectivity',reflectivityMap, ...
-    'ReflectivityMap',reflectivityType);
-
-% Create a radar looking to the right
-mountAngles = [0 depang 0];
-rdr = radarTransceiver('MountingAngles',mountAngles,'NumRepetitions',1);
-
-% Set peak power
-rdr.Transmitter.PeakPower = 50e3; 
-
-% Set receiver sample rate and noise figure
-rdr.Receiver.SampleRate = fs;
-rdr.Receiver.NoiseFigure = 30; 
-
-% Define transmit and receive antenna and corresponding parameters
-antbw = ap2beamwidth(apertureLength,lambda); 
-ant = phased.SincAntennaElement('FrequencyRange',[1e9 10e9],'Beamwidth',antbw);
-rdr.TransmitAntenna.Sensor = ant;
-rdr.TransmitAntenna.OperatingFrequency = freq;
-rdr.ReceiveAntenna.Sensor = ant;
-rdr.ReceiveAntenna.OperatingFrequency = freq;
-antennaGain = aperture2gain(apertureLength^2,lambda); 
-rdr.Transmitter.Gain = antennaGain;
-rdr.Receiver.Gain = antennaGain;
-
-% Configure the LFM signal of the radar
-rdr.Waveform = phased.LinearFMWaveform('SampleRate',fs,'PulseWidth',tpd, ...
-    'PRF',prf,'SweepBandwidth',bw); 
-
-% Add radar to radar platform
-rdrplat.Sensors = rdr;
-
-% Collect clutter returns with the clutterGenerator
-clutterGenerator(scene,rdr); 
-
-% Initialize output IQ datacube
-minSample = 500; % Minimum sample range
-maxRange = 2500; % Maximum range for IQ collection
-truncRngSamp = ceil(range2time(maxRange)*fs); % Limit the number of samples
-T = 1/prf; % Pulse repetition interval (sec)
-numPulses = dur/T + 1; % Number of pulses 
-raw = zeros(numel(minSample:truncRngSamp),numPulses); % IQ datacube 
-
-% Collect IQ 
-ii = 1; 
-hRaw = helperPlotRawIQ(raw,minSample);
-% Simulate IQ
-while advance(scene) %#ok<UNRCH>
-    tmp = receive(scene); % nsamp x 1
-    raw(:,ii) = tmp{1}(minSample:truncRngSamp);
-    if mod(ii,100) == 0 % Update plot after 100 pulses
-        helperUpdatePlotRawIQ(hRaw,raw);
+for circleOfFourSize = 4:4
+    disp("round " + circleOfFourSize)
+    rdrpos1 = [0 -v rdrhgt];           % Start position of the radar (m)
+    rdrvel = [0 v 0];                  % Radar plaform velocity
+    if circleOfFourSize == 2
+        rdrpos1 = [1000 1000 rdrhgt];
+        rdrvel = [v 0 0];
+    elseif circleOfFourSize == 3
+        rdrpos1 = [2200 v rdrhgt];
+        rdrvel = [0 -v 0];
+    elseif circleOfFourSize == 4
+        rdrpos1 = [1200 -1000 rdrhgt];
+        rdrvel = [-v 0 0];
     end
-    ii = ii + 1;
-end
-helperUpdatePlotRawIQ(hRaw,raw);
-
-% Generating Single Look Complex image using range migration algorithm
-slcimg = rangeMigrationLFM(raw,rdr.Waveform,freq,v,rc);
-helperPlotSLC(slcimg,minSample,fs,v,prf,rdrpos1,targetpos, ...
-    xvec,yvec,A)
-
-for it = 1:3
-    % Determine whether the target was occluded
-    occ = false(1,numPulses); 
-    for ip = 1:numPulses
-        rdrpos = rdrpos1 + rdrvel.*1/prf*(ip - 1); 
-        occ(ip) = s.occlusion(rdrpos,targetpos(it,:));
+    rdrpos2 = rdrvel*dur + rdrpos1;    % End position of the radar (m)
+    
+    % Antenna orientation
+    depang = depressionang(rdrhgt,rc,'Flat','TargetHeight',mean(tgthgts)); % Depression angle (deg)
+    grazang = depang; % Grazing angle (deg)
+    
+    % Azimuth resolution
+    azResolution = sarazres(rc,lambda,len)  % Cross-range resolution (m)
+    
+    % Determine PRF bounds 
+    [swlen,swwidth] = aperture2swath(rc,lambda,apertureLength,grazang);
+    [prfmin,prfmax] = sarprfbounds(v,azResolution,swlen,grazang)
+    
+    % Select a PRF within the PRF bounds
+    prf = 500; % Pulse repetition frequency (Hz)
+    
+    % Create a radar scenario
+    scene = radarScenario('UpdateRate',prf,'IsEarthCentered',false,'StopTime',dur);
+    
+    % Add platforms to the scene using the configurations previously defined 
+    rdrplat = platform(scene,'Trajectory',kinematicTrajectory('Position',rdrpos1,'Velocity',[0 v 0]));
+    
+    % Add target platforms
+    rcs = rcsSignature('Pattern',5); 
+    for it = 1:3
+        platform(scene,'Position',targetpos(it,:),'Signatures',{rcs});
     end
-
-    % Translate occlusion values to a visibility status
-    helperGetVisibilityStatus(it,occ)
+    
+    % Plot ground truth
+    helperPlotGroundTruth(xvec,yvec,A,rdrpos1,rdrpos2,targetpos)
+    
+    % Add land surface to scene
+    s = landSurface(scene,'Terrain',A,'Boundary',[xLimits;yLimits], ...
+        'RadarReflectivity',reflectivityMap, ...
+        'ReflectivityMap',reflectivityType);
+    
+    % Create a radar looking to the right / down / left / up
+    mountAngles = [0 depang 0];
+    if circleOfFourSize == 2
+        mountAngles = [-90 depang 0];
+    elseif circleOfFourSize == 3
+        mountAngles = [0 180-depang 0];
+    elseif circleOfFourSize == 4
+        mountAngles = [90 depang 0];
+    end
+    
+    rdr = radarTransceiver('MountingAngles',mountAngles,'NumRepetitions',1);
+    
+    % Set peak power
+    rdr.Transmitter.PeakPower = 50e3; 
+    
+    % Set receiver sample rate and noise figure
+    rdr.Receiver.SampleRate = fs;
+    rdr.Receiver.NoiseFigure = 30; 
+    
+    % Define transmit and receive antenna and corresponding parameters
+    antbw = ap2beamwidth(apertureLength,lambda); 
+    ant = phased.SincAntennaElement('FrequencyRange',[1e9 10e9],'Beamwidth',antbw);
+    rdr.TransmitAntenna.Sensor = ant;
+    rdr.TransmitAntenna.OperatingFrequency = freq;
+    rdr.ReceiveAntenna.Sensor = ant;
+    rdr.ReceiveAntenna.OperatingFrequency = freq;
+    antennaGain = aperture2gain(apertureLength^2,lambda); 
+    rdr.Transmitter.Gain = antennaGain;
+    rdr.Receiver.Gain = antennaGain;
+    
+    % Configure the LFM signal of the radar
+    rdr.Waveform = phased.LinearFMWaveform('SampleRate',fs,'PulseWidth',tpd, ...
+        'PRF',prf,'SweepBandwidth',bw); 
+    
+    % Add radar to radar platform
+    rdrplat.Sensors = rdr;
+    
+    % Collect clutter returns with the clutterGenerator
+    clutterGenerator(scene,rdr); 
+    
+    % Initialize output IQ datacube
+    minSample = 500; % Minimum sample range
+    maxRange = 2500; % Maximum range for IQ collection
+    truncRngSamp = ceil(range2time(maxRange)*fs); % Limit the number of samples
+    T = 1/prf; % Pulse repetition interval (sec)
+    numPulses = dur/T + 1; % Number of pulses 
+    raw = zeros(numel(minSample:truncRngSamp),numPulses); % IQ datacube 
+    
+    % Collect IQ 
+    ii = 1; 
+    hRaw = helperPlotRawIQ(raw,minSample);
+    % Simulate IQ
+    while advance(scene) %#ok<UNRCH>
+        tmp = receive(scene); % nsamp x 1
+        raw(:,ii) = tmp{1}(minSample:truncRngSamp);
+        if mod(ii,100) == 0 % Update plot after 100 pulses
+            helperUpdatePlotRawIQ(hRaw,raw);
+        end
+        ii = ii + 1;
+    end
+    helperUpdatePlotRawIQ(hRaw,raw);
+    
+    % Generating Single Look Complex image using range migration algorithm
+    slcimg = rangeMigrationLFM(raw,rdr.Waveform,freq,v,rc);
+    helperPlotSLC(slcimg,minSample,fs,v,prf,rdrpos1,targetpos, ...
+        xvec,yvec,A)
 end
 
+%%%
+%%%for it = 1:3
+%%%    % Determine whether the target was occluded
+%%%    occ = false(1,numPulses); 
+%%%    for ip = 1:numPulses
+%%%        rdrpos = rdrpos1 + rdrvel.*1/prf*(ip - 1); 
+%%%        occ(ip) = s.occlusion(rdrpos,targetpos(it,:));
+%%%    end
+%%%
+%%%    % Translate occlusion values to a visibility status
+%%%    helperGetVisibilityStatus(it,occ)
+%%%end
+%%%
